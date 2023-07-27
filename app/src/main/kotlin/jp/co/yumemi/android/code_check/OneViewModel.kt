80dc4a331e96ee9df784a94a95ef5a3499a87e34
waigoma <48357625+waigoma@users.noreply.github.com>
2023-07-27 17:57:35 +0900

    Revert "Feature/#12 dark mode"

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -1,18 +1,17 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.codecheck
+package jp.co.yumemi.android.code_check
 
+import android.content.Context
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
-import io.ktor.client.HttpClient
-import io.ktor.client.call.receive
-import io.ktor.client.engine.android.Android
-import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.client.request.parameter
-import io.ktor.client.statement.HttpResponse
-import jp.co.yumemi.android.codecheck.MainActivity.Companion.lastSearchDate
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -21,45 +20,35 @@ import org.json.JSONObject
 import java.util.*
 
 /**
- * RepoViewFragment で使う
+ * TwoFragment で使う
  */
-class RepoViewModel : ViewModel() {
+class OneViewModel(
+    val context: Context
+) : ViewModel() {
 
     // 検索結果
-    fun searchResults(inputText: String): List<Item> = runBlocking {
+    fun searchResults(inputText: String): List<item> = runBlocking {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
             }
 
             val jsonBody = JSONObject(response.receive<String>())
-            val jsonItems = jsonBody.optJSONArray("items")
-            val items = mutableListOf<Item>()
 
-            // 検索結果がない場合
-            if (jsonItems == null) {
-                items.add(
-                    Item(
-                        name = "検索結果がありません",
-                        ownerIconUrl = "",
-                        language = "",
-                        stargazersCount = 0,
-                        watchersCount = 0,
-                        forksCount = 0,
-                        openIssuesCount = 0,
-                    ),
-                )
-                return@async items.toList()
-            }
+            val jsonItems = jsonBody.optJSONArray("items")!!
 
-            // 検索結果がある場合
+            val items = mutableListOf<item>()
+
+            /**
+             * アイテムの個数分ループする
+             */
             for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)
+                val jsonItem = jsonItems.optJSONObject(i)!!
                 val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url") ?: "avatar_url がありません"
+                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
@@ -67,15 +56,15 @@ class RepoViewModel : ViewModel() {
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
-                    Item(
+                    item(
                         name = name,
                         ownerIconUrl = ownerIconUrl,
-                        language = language,
+                        language = context.getString(R.string.written_language, language),
                         stargazersCount = stargazersCount,
                         watchersCount = watchersCount,
                         forksCount = forksCount,
-                        openIssuesCount = openIssuesCount,
-                    ),
+                        openIssuesCount = openIssuesCount
+                    )
                 )
             }
 
@@ -87,7 +76,7 @@ class RepoViewModel : ViewModel() {
 }
 
 @Parcelize
-data class Item(
+data class item(
     val name: String,
     val ownerIconUrl: String,
     val language: String,
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
@@ -1,14 +1,14 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.codecheck
+package jp.co.yumemi.android.code_check
 
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
+class TopActivity : AppCompatActivity(R.layout.activity_top) {
 
     companion object {
-        var lastSearchDate: Date = Date()
+        lateinit var lastSearchDate: Date
     }
 }

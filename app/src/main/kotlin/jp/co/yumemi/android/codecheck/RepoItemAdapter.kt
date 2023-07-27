0be031b802a62b45c6a7361db694b358b9152da0
waigoma <waichi0302@gmail.com>
2023-07-27 21:00:12 +0900

    RepoSearchFragment と Adapter を分離し、Adapter の名前を変更

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepoItemAdapter.kt
@@ -0,0 +1,46 @@
+package jp.co.yumemi.android.codecheck
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+private val diff_util = object : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class RepoItemAdapter (
+    private val itemClickListener: OnItemClickListener,
+) : ListAdapter<Item, RepoItemAdapter.ViewHolder>(diff_util) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener {
+        fun itemClick(item: Item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        val repoNameView = holder.itemView.findViewById<View>(R.id.repositoryNameView)
+        if (repoNameView is TextView) repoNameView.text = item.name
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
+    }
+}

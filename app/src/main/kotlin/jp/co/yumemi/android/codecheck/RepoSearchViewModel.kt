8c25bb6aaa1019b1efc80c17a5866ccbf457f83e
waigoma <waichi0302@gmail.com>
2023-07-27 21:37:54 +0900

    検索語が無いときの処理を追加し、コルーチン処理を GlobalScope から viewModelScope に変更

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepoSearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepoSearchViewModel.kt
@@ -5,6 +5,7 @@ package jp.co.yumemi.android.codecheck
 
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.engine.android.Android
@@ -13,10 +14,11 @@ import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
 import jp.co.yumemi.android.codecheck.MainActivity.Companion.lastSearchDate
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.parcelize.Parcelize
+import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 
@@ -24,65 +26,77 @@ import java.util.*
  * RepoViewFragment で使う
  */
 class RepoSearchViewModel : ViewModel() {
+    private val nothingItem = listOf(
+        Item(
+            name = "検索結果がありません",
+            ownerIconUrl = "",
+            language = "",
+            stargazersCount = 0,
+            watchersCount = 0,
+            forksCount = 0,
+            openIssuesCount = 0,
+        ))
 
     // 検索結果
     fun searchResults(inputText: String): List<Item> = runBlocking {
+        if (inputText.isEmpty()) return@runBlocking nothingItem
+
         val client = HttpClient(Android)
+        viewModelScope.async(Dispatchers.IO) {
+            try {
+                val response: HttpResponse =
+                    client.get("https://api.github.com/search/repositories") {
+                        header("Accept", "application/vnd.github.v3+json")
+                        parameter("q", inputText)
+                    }
 
-        return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
+                val jsonBody = JSONObject(response.receive<String>())
+                val jsonItems = jsonBody.optJSONArray("items")
+                val items = mutableListOf<Item>()
 
-            val jsonBody = JSONObject(response.receive<String>())
-            val jsonItems = jsonBody.optJSONArray("items")
-            val items = mutableListOf<Item>()
+                // 検索結果がない場合
+                if (jsonItems == null) {
+                    return@async nothingItem
+                }
 
-            // 検索結果がない場合
-            if (jsonItems == null) {
-                items.add(
-                    Item(
-                        name = "検索結果がありません",
-                        ownerIconUrl = "",
-                        language = "",
-                        stargazersCount = 0,
-                        watchersCount = 0,
-                        forksCount = 0,
-                        openIssuesCount = 0,
-                    ),
-                )
+                // 検索結果から Item を作成する
+                jsonItems.forEach {
+                    val name = it.optString("full_name")
+                    val ownerIconUrl = it.optJSONObject("owner")?.optString("avatar_url") ?: "avatar_url がありません"
+                    val language = it.optString("language")
+                    val stargazersCount = it.optLong("stargazers_count")
+                    val watchersCount = it.optLong("watchers_count")
+                    val forksCount = it.optLong("forks_conut")
+                    val openIssuesCount = it.optLong("open_issues_count")
+
+                    items.add(
+                        Item(
+                            name = name,
+                            ownerIconUrl = ownerIconUrl,
+                            language = language,
+                            stargazersCount = stargazersCount,
+                            watchersCount = watchersCount,
+                            forksCount = forksCount,
+                            openIssuesCount = openIssuesCount,
+                        ),
+                    )
+                }
+
+                lastSearchDate = Date()
                 return@async items.toList()
+            } catch (e: Exception) {
+                return@async nothingItem
             }
-
-            // 検索結果がある場合
-            for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url") ?: "avatar_url がありません"
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                items.add(
-                    Item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = language,
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount,
-                    ),
-                )
-            }
-
-            lastSearchDate = Date()
-
-            return@async items.toList()
         }.await()
+    }
+
+    /**
+     * JSONArray を forEach できるようにする
+     */
+    private fun JSONArray.forEach(action: (JSONObject) -> Unit) {
+        for (i in 0 until length()) {
+            action(getJSONObject(i))
+        }
     }
 }
 
